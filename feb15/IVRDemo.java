6654f2078ffd03d9344aa154fdb6ec86239f50f2
pragra <sunny@pragra.io>
2025-02-15 13:42:00 -0500

    IVR demo

--- feb15/IVRDemo.java
+++ feb15/IVRDemo.java
@@ -1,7 +1,40 @@
 package feb15;
 
+import java.util.Scanner;
+
 public class IVRDemo {
     public static void main(String[] args) {
+
+
+        System.out.println("*******************");
+        System.out.println("Welcome to My Telecom");
+        System.out.println("*******************");
+
+        System.out.println("1. English");
+        System.out.println("2. French");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your language input");
+        int language = scanner.nextInt();
+
+        if(language == 1){
+
+
+
+        }else if (language == 2){
+
+        }else {
+            System.out.println("Invalid input ");
+        }
+
+
+
+
+
+
+
+
+
 
 
         // customer service, rogers, bell, telus
