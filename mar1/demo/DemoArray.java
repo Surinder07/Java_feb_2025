ec103926f3dadebd09c61c84d354da1e448d8325
pragra <sunny@pragra.io>
2025-03-01 14:24:44 -0500

    Arrays

---
+++ mar1/demo/DemoArray.java
@@ -0,0 +1,54 @@
+package mar1.demo;
+
+public class DemoArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10,20,30,40,50,60,70,80,90, 100};
+        System.out.println(arr[9]);
+        System.out.println(arr[0]);
+
+        int x = 10;
+        int y = 20;
+
+        // some opearion
+        // x - 20 , y = 10
+        int temp = x;
+        x = y;
+        y = temp;
+
+    }
+}
+
+
+
+/*
+
+
+total size = 10 , length
+0 - 9
+
+
+reverese the array
+
+find the max/ min element from the array
+                    max = 235
+                10   43   230   23    235   234   55
+
+        int max = arr[0];
+        if(arr[i] > arr[0] ){
+
+            max = arr[i];
+        }
+
+return max;
+
+min.
+
+
+
+
+
+
+
+ */
