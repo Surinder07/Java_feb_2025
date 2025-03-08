21c39fc3a1ee43e11dfcc268805bc08e83c81db9
pragra <sunny@pragra.io>
2025-03-08 11:07:06 -0500

    StringBuffer

---
+++ mar8/Employee.java
@@ -0,0 +1,27 @@
+package mar8;
+
+public class Employee {
+    int id;
+    String name;
+    double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+
+    public void test(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ out/production/java_feb_2025/mar8/readme.md
@@ -0,0 +1,31 @@
+## Recap 
+
+
+1. Strings 
+2. memory - stack / heap 
+3. Arrays - 1d and 2d array 
+
+
+explain 
+
+String s = "Pragra";  // String constant pool 
+String s1 = new String("Pragra");
+in terms of memory 
+
+
+2. why Strings are immutable 
+
+Mutable String s
+
+
+
+
+
+
+
+
+
+
+
+
+
